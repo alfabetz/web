9b4dc4616f2a0e448f4798f337f093c152e66502
Vitaliy <vitaliy.maystruk@gmail.com>
2020-09-07 17:53:46 +0300

    Create my first great project

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
-<?php
-echo "Hello, NIX Education";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>My first project</title>
+</head>
+<body>
+<header></header>
+<nav>
+	<ul>
+		<li><a href="#">урок 1</a></li>
+		<li><a href="#">урок 2</a></li>
+	</ul>
+</nav>
+<main><main>
+<footer></footer>
+</body>
+</html>
